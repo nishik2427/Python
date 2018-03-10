e0353fb3342aa76128b8a590c70ddc0af9e24894
nishik2427 <KOJISERVER@DESKTOP-P9KPCJO>
2018-03-10 22:01:30 +0900

    テスト

---
+++ sound_pygame.py
@@ -0,0 +1,14 @@
+import pygame.mixer
+import time
+
+def main():
+    pygame.mixer.init(frequency = 44100 )
+    pygame.mixer.music.load("警報音声.wav")        # 音楽ファイルの読み込み
+    pygame.mixer.music.play(-1)                   # 音楽の再生回数(ループ再生)
+    time.sleep(10)                                # 音楽の再生時間
+    pygame.mixer.music.stop()                     # 再生の終了
+
+
+if __name__ == '__main__':
+    main()
+
